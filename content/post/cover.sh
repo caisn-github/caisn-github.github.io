b0a8102e9bcca422c982eda1d3980579c1aba734
root <root@sinan_computer.localdomain>
2024-12-07 17:45:28 +0800

    mv images

---
+++ content/post/cover.sh
@@ -0,0 +1,44 @@
+#!bin/bash
+function readFile() {
+   cat 1.1 | while read line
+      do
+              echo ${line}
+              len2=${#line}
+              echo ${len2}
+              echo ${line: 0: len2-3}
+              mkdir ${line: 0: len2-3}
+              cp *.png ${line: 0: len2-3}
+              # echo [${path}]
+              ## mkdir ./path
+      done
+}
+
+function mvImage() 
+{
+      rm 1.1
+      find ./ -name "*.md" >> 1.1
+      if [ ! -s 1.1 ];
+      then echo "hello";
+          else
+      readFile
+      fi
+
+      rm *.png
+      rm 1.1
+}
+
+function read_dir() {
+	for subDir in  `ls $1`
+	do 
+		if [ -d $1"/"$subDir ]
+		then 
+	       	        echo ${subDir}
+			read_dir $1"/"$subDir
+                        cd $subDir
+			mvImage
+		fi
+	done
+}
+
+read_dir $1
+
